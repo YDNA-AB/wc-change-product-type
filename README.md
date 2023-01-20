de7e137d02129a617188695edf45b6e7369121a6
Jesper Gustafsson <darkclow3r@gmail.com>
2023-01-20 12:28:13 +0100

    feat add new endpoint & add some request validation & rewrite docs

--- README.md
+++ README.md
@@ -1,24 +1,86 @@
-# Change the product type to variation
+# Woo Change Product Type
 
-- Enables users to change product type to variation via the rest api.
-- It uses the standard woocommerce api authentication to authenticate the request.
+This is a plugin that adds a few new endpoints to the REST API.
 
-It requires you to provide the `product_id` of the product you want to change and also the `parent_id` of the product that is supposed to be the parent.
+_Note:_ It uses the standard WooCommerce API authentication to authenticate requests.
 
-Send a POST request to https://yourdomain.com/wp-json/wc/v3/ydna/change-product-type
+# API Endpoints
 
-Example body:
+## `/change-product-type`
+
+```
+URL: https://example.com/wp-json/wc/v3/ydna/change-product-type
+Method: POST
+```
+
+__Request body type definition:__
+
+```ts
+/** The request body (defined in TypeScript). */
+type RequestBody = {
+  /** Array of products to change the parent (and type) of. */
+  products: {
+    /** Product ID of the product to change. */
+    product_id: number;
+    /** Product ID of the product to set as the parent. */
+    parent_id: number;
+  }[];
+}
+```
+
+__Example request body:__
+
 ```json
 {
   "products": [
     {
       "product_id": 123,
       "parent_id": 987
-     },
-     {
+    },
+    {
       "product_id": 654,
       "parent_id": 321
-      }
-   ]
+    }
+  ]
 }
 ```
+
+Updates one or more products at once.
+
+It requires you to provide the `product_id` of the product you want to change and also the `parent_id` of the product that is supposed to be the parent.
+
+__Purpose:__ It is not possible to change the `type` of a product to `variation` using the WooCommerce REST API or user interface.
+
+## `/change-product-type-to-simple`
+
+```
+URL: https://example.com/wp-json/wc/v3/ydna/change-product-type-to-simple
+Method: POST
+```
+
+__Request body type definition:__
+
+```ts
+/** The request body (defined in TypeScript). */
+type RequestBody = {
+  /** Product IDs of products to change. Sets their 'type' to "simple" and removes their parent. */
+  product_ids: number[];
+}
+```
+
+__Example request body:__
+
+```json
+{
+  "product_ids": [
+    123,
+    654
+  ]
+}
+```
+
+Updates one or more products at once.
+
+It sets the `type` to `"simple"` and removes the parent (if there is any). It does nothing unless the product is a variation product.
+
+__Purpose:__ It is not possible to change the `type` of a product to `simple` (if they are of type `variation`) using the WooCommerce REST API or user interface.
--- woo-change-product-type.php
+++ woo-change-product-type.php
@@ -16,17 +16,25 @@ function create_product_type_route() {
         'methods' => 'POST',
         'permission_callback' => 'authenticate_request',
         'callback' => 'change_product_type_callback',
-) );
+    ) );
+
+    register_rest_route( $name_space, 'change-product-type-to-simple', array(
+        'methods' => 'POST',
+        'permission_callback' => 'authenticate_request',
+        'callback' => 'change_product_type_to_simple_callback',
+    ) );
 }
 
 function authenticate_request( WP_REST_Request $request ) {
-	if ( ! current_user_can( 'edit_posts') ) {
+	if ( ! current_user_can( 'edit_posts' ) ) {
 		return new WP_Error( 'invalid_consumer_key', 'Consumer key or secret is missing or wrong', array( 'status' => 403 ) );
 	}
     return true;
 }
 
 function change_product_type_callback( WP_REST_Request $request ) {
+    // Read and parse the body
+
     $body = $request->get_body();
     $data = json_decode( $body, true );
 
@@ -34,25 +42,49 @@ function change_product_type_callback( WP_REST_Request $request ) {
         return new WP_Error( 'invalid_json', 'The request body is not a valid JSON format', array( 'status' => 400 ) );
     }
 
+    // Validate request body
+
     if ( !isset( $data['products'] ) || !is_array( $data['products'] ) ) {
-        return new WP_Error( 'invalid_json', 'The request body does not contain a products array', array( 'status' => 400 ) );
+        // @TODO Make sure that the error code is correct.
+        return new WP_Error( 'invalid_json', '"products" is missing in the request body (or the value is of an invalid type).', array( 'status' => 400 ) );
     }
+
+    foreach ( $data['products'] as $index => $product ) {
+        $missing_product_id = !isset($product['product_id']);
+        $missing_parent_id  = !isset($product['parent_id']);
+
+        if ( $missing_product_id || $missing_parent_id ) {
+            $error_str = '';
+            if ( $missing_product_id ) { $error_str .= '"product_id"'; }
+            if ( $missing_parent_id) {
+                if ( $error_str != '' ) { $error_str .= ' and '; }
+                $error_str .= '"parent_id"';
+            }
+            
+            // @TODO Make sure that the error code is correct.
+            return new WP_Error( 'invalid_json', $error_str . ' is missing in the request body (or the value is of an invalid type). See "products[' . $index . ']".', array( 'status' => 400 ) );
+        }
+    }
+
+    // Update products
 
     global $wpdb;
     $table_name = $wpdb->prefix . 'posts';
     $update_count = 0;
     foreach ( $data['products'] as $product ) {
         $product_id = $product['product_id'];
-        $parent_id = $product['parent_id'];
+        $parent_id  = $product['parent_id'];
 
         $update_count += $wpdb->update(
             $table_name,
             array( 
                 'post_parent' => $parent_id,
-                'post_type' => 'product_variation' ),
+                'post_type' => 'product_variation'
+            ),
             array(
                 'ID' => $product_id,
-                'post_type' => 'product' )
+                'post_type' => 'product'
+            )
         );
     }
 
@@ -63,6 +95,65 @@ function change_product_type_callback( WP_REST_Request $request ) {
     if ( count($data['products']) !== $update_count ) {
         return new WP_Error( 'update_query_failed', 'Update query failed, the number of updated rows is not equal to the number of products', array( 'status' => 500 ) );
     }
+
+    // Success!
+
+    return true;
+}
+
+function change_product_type_to_simple_callback( WP_REST_Request $request ) {
+    // Read and parse the body
+
+    $body = $request->get_body();
+    $data = json_decode( $body, true );
+
+    if ( json_last_error() !== JSON_ERROR_NONE ) {
+        return new WP_Error( 'invalid_json', 'The request body is not a valid JSON format', array( 'status' => 400 ) );
+    }
+
+    // Validate request body
+
+    if ( !isset( $data['product_ids'] ) || !is_array( $data['product_ids'] ) ) {
+        // @TODO Make sure that the error code is correct.
+        return new WP_Error( 'invalid_json', '"product_ids" is missing in the request body (or the value is of an invalid type).', array( 'status' => 400 ) );
+    }
+
+    foreach ( $data['product_ids'] as $index => $product_id ) {
+        if ( !is_int($product_id) ) {
+            // @TODO Make sure that the error code is correct.
+            return new WP_Error( 'invalid_json', 'One or more product IDs in the request body is not an integer. See "product_ids[' . $index . ']".', array( 'status' => 400 ) );
+        }
+    }
+
+    // Update products
+
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'posts';
+    $update_count = 0;
+    foreach ( $data['product_ids'] as $product_id ) {
+        $update_count += $wpdb->update(
+            $table_name,
+            array( 
+                'post_parent' => null,
+                'post_type' => 'product'
+            ),
+            array(
+                'ID' => $product_id,
+                'post_type' => 'product_variation'
+            )
+        );
+    }
+
+    if ( false === $update_count ) {
+        return new WP_Error( 'update_query_failed', 'Update query failed' . $wpdb->last_error, array( 'status' => 500 ) );
+    }
+
+    if ( count($data['product_ids']) !== $update_count ) {
+        return new WP_Error( 'update_query_failed', 'Update query failed, the number of updated rows is not equal to the number of products', array( 'status' => 500 ) );
+    }
+
+    // Success!
+
     return true;
 }
 
